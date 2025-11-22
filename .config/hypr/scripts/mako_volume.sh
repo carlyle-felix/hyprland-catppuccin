4b071066166e637b26eb9cf8c17fbd169600bd40
carlyle-felix <carlyle.felix@gmail.com>
2025-11-22 20:30:38 +0200

    hypr: modify mako_volume.sh

--- .config/hypr/scripts/mako_volume.sh
+++ .config/hypr/scripts/mako_volume.sh
@@ -2,6 +2,49 @@
 
 iDIR="$HOME/.config/mako/icons"
 
+main() {
+
+	arg=$1
+
+	# Execute accordingly
+	case ${arg} in
+		"--get")
+			get_volume
+			;;
+		"--inc")
+			get_mute_state "1"
+			if [ $? -eq 1 ]; then	
+				notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-mute.png" "    Muted"
+			else
+				inc_volume
+			fi
+			;;
+		"--dec")
+			get_mute_state "1"
+			if [ $? -eq 1 ]; then	
+				notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-mute.png" "    Muted"
+			else
+				dec_volume
+			fi
+			;;
+		"--toggle")
+			toggle_mute
+			;;
+		"--toggle-mic")
+			toggle_mic
+			;;
+		"--mic-inc")
+			inc_mic_volume
+			;;
+		"--mic-dec")
+			dec_mic_volume
+			;;
+		*)
+			get_volume
+			;;
+	esac
+}
+
 # Get Volume
 get_volume() {
 	volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
@@ -11,88 +54,51 @@ get_volume() {
 	echo "${volume}"
 }
 
-# Get icons
-get_icon() {
-	current=$(get_volume)
-	if [[ "$current" -eq "0" ]]; then
-		echo "$iDIR/volume-mute.png"
-	elif [[ ("$current" -ge "0") && ("$current" -le "30") ]]; then
-		echo "$iDIR/volume-low.png"
-	elif [[ ("$current" -ge "30") && ("$current" -le "60") ]]; then
-		echo "$iDIR/volume-mid.png"
-	elif [[ ("$current" -ge "60") && ("$current" -le "100") ]]; then
-		echo "$iDIR/volume-high.png"
-	fi
-}
+get_mute_state() {
+	arg="$1"
 
-# Notify
-notify_user() {
-	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Volume : $(get_volume) %"
+	if [ $arg -eq 0 ]; then
+		dev="@DEFAULT_AUDIO_SOURCE@"
+	elif [ $arg -eq 1 ]; then
+		dev="@DEFAULT_AUDIO_SINK@"
+	fi
+
+	wpctl get-volume ${dev} | grep "MUTED"
+	if [ $? -eq 1 ]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 # Increase Volume
 inc_volume() {
-	wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+ && notify_user
+	wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+ && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-up.png" "    Volume: $(get_volume) %"
 }
 
 # Decrease Volume
 dec_volume() {
-	wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- && notify_user
+	wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-down.png" "    Volume: $(get_volume) %"
 }
 
 # Toggle Mute
 toggle_mute() {
-	wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep "MUTED"
-	if [ $? -eq 1 ]; then
-		wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-mute.png" "Mute"
+	get_mute_state "1"
+	if [ $? -eq 0 ]; then
+		wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-mute.png" "    Mute"
 	else
-		wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-mute.png" "Unmute"
+		wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/volume-unmute.png" "    Unmute"
 	fi
 }
 
 # Toggle Mic
-#toggle_mic() {
-#	wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/microphone-mute.png" "Microphone Switched OFF"
-#
-#}
-
-# Get icons
-get_mic_icon() {
-	current=$(pamixer --default-source --get-volume)
-	if [[ "$current" -eq "0" ]]; then
-		echo "$iDIR/microphone.png"
-	elif [[ ("$current" -ge "0") && ("$current" -le "30") ]]; then
-		echo "$iDIR/microphone.png"
-	elif [[ ("$current" -ge "30") && ("$current" -le "60") ]]; then
-		echo "$iDIR/microphone.png"
-	elif [[ ("$current" -ge "60") && ("$current" -le "100") ]]; then
-		echo "$iDIR/microphone.png"
+toggle_mic() {
+	get_mute_state "0"
+	if [ $? -eq 0 ]; then
+		wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/microphone-mute.png" "    Microphone: OFF"
+	else
+		wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$iDIR/microphone.png" "    Microphone: ON"
 	fi
 }
-# Notify
-notify_mic_user() {
-	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_mic_icon)" "Mic-Level : $(pamixer --default-source --get-volume) %"
-}
 
-# Execute accordingly
-if [[ "$1" == "--get" ]]; then
-	get_volume
-elif [[ "$1" == "--inc" ]]; then
-	inc_volume
-elif [[ "$1" == "--dec" ]]; then
-	dec_volume
-elif [[ "$1" == "--toggle" ]]; then
-	toggle_mute
-elif [[ "$1" == "--toggle-mic" ]]; then
-	toggle_mic
-elif [[ "$1" == "--get-icon" ]]; then
-	get_icon
-elif [[ "$1" == "--get-mic-icon" ]]; then
-	get_mic_icon
-elif [[ "$1" == "--mic-inc" ]]; then
-	inc_mic_volume
-elif [[ "$1" == "--mic-dec" ]]; then
-	dec_mic_volume
-else
-	get_volume
-fi
+main $@
