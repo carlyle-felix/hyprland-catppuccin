eaec65511df67a19a0e1be415e27e80a8499a8d7
carlyle-felix <carlyle.felix@gmail.com>
2025-11-22 16:18:30 +0200

    .config/waybar: modify pipewire module and update syle.css

--- .config/waybar/scripts/pipewire.sh
+++ .config/waybar/scripts/pipewire.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#
+#   Modified script to output json line for use with `format-icons` and class in style.css
+#   Original script: https://github.com/Alexays/Waybar/wiki/Module:-Custom:-Examples
+#
 
 set -e
-
-ARG=$1
 
 # https://blog.dhampir.no/content/sleeping-without-a-subprocess-in-bash-and-how-to-sleep-forever
 snore() {
@@ -18,23 +20,25 @@ while snore $DELAY; do
 
     if [[ $WP_OUTPUT =~ ^Volume:[[:blank:]]([0-9]+)\.([0-9]{2})([[:blank:]].MUTED.)?$ ]]; then
         if [[ -n ${BASH_REMATCH[3]} ]]; then
-            printf "    \n"
+            CLASS="mute"
+
+            echo "{\"tooltip\": \"Volume: muted\", \"alt\": \"$CLASS\", \"class\": \"$CLASS\", \"percentage\": \"\"}"
+
         else
             VOLUME=$((10#${BASH_REMATCH[1]}${BASH_REMATCH[2]}))
-            ICON=(
-                "   "
-                "   "
-                "   "
-            )
 
-            if [[ $VOLUME -gt 50 ]]; then
-                printf "${ICON[0]}  \n"
-            elif [[ $VOLUME -gt 25 ]]; then
-                printf "${ICON[1]}  \n"
+            if [[ $VOLUME -gt 65 ]]; then
+                CLASS="high"
+            elif [[ $VOLUME -gt 35 ]]; then
+                CLASS="medium"
             elif [[ $VOLUME -ge 0 ]]; then
-                printf "${ICON[2]}  \n"
+                CLASS="low"
             fi
+
+            echo "{\"tooltip\": \"Volume: $VOLUME%\", \"alt\": \"$CLASS\", \"class\": \"$CLASS\", \"percentage\": $VOLUME}"
+
         fi
+
     fi
 done
 
